5a3a0b3b9544c650c72e7fdf4c88450d77a42a83
gmozart <mozart.gleison@gmail.com>
2023-05-16 02:02:55 -0300

    Removing dirty code

--- src/main/java/com/api/dvdrental/entity/language/LanguageRepository.java
+++ src/main/java/com/api/dvdrental/entity/language/LanguageRepository.java
@@ -3,4 +3,7 @@ package com.api.dvdrental.entity.language;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LanguageRepository extends JpaRepository<Language, Long> {
+
+
+
 }
